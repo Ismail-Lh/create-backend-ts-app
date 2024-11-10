13a43d71cb0a5c96a534308ce6da01a813ca782f
Ismail-Lh <ismailsaad03@gmail.com>
2024-11-10 13:59:02 +0100

    Replace inquirer with @clack/prompts for project type and Prettier confirmation prompts

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,36 +1,33 @@
-import inquirer from 'inquirer';
+import { confirm, select } from '@clack/prompts';
 
 /**
- * Prompts the user to choose the type of project they want to create.
+ * Prompts the user to select the type of project they want to create.
  *
  * @returns {Promise<'typescript' | 'javascript'>} A promise that resolves to the selected project type.
- *
  */
 export async function promptForProjectType(): Promise<
   'typescript' | 'javascript'
 > {
-  const answer = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'projectType',
-      message: 'Choose the project type:',
-      choices: ['TypeScript', 'JavaScript'],
-      default: 'TypeScript',
-    },
-  ]);
+  const projectType = await select({
+    message: 'Choose the project type:',
+    options: [
+      { value: 'typescript', label: 'TypeScript' },
+      { value: 'javascript', label: 'JavaScript' },
+    ],
+  });
 
-  return answer.projectType.toLowerCase() as 'typescript' | 'javascript';
+  return projectType as 'typescript' | 'javascript';
 }
 
+/**
+ * Prompts the user to confirm if they want to use Prettier for code formatting.
+ *
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating the user's choice.
+ */
 export async function promptForPrettier(): Promise<boolean> {
-  const answer = await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'usePrettier',
-      message: 'Do you want to use Prettier for code formatting?',
-      default: true,
-    },
-  ]);
+  const usePrettier = await confirm({
+    message: 'Do you want to use Prettier for code formatting?',
+  });
 
-  return answer.usePrettier;
+  return usePrettier as boolean;
 }
